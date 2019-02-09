3856c882d51a389346b0ad2abb340d2c196b9d99
Karishma Upadhyay <41079940+karishma10091993@users.noreply.github.com>
2019-02-09 17:01:16 +0530

    Add files via upload

---
+++ AppDelegate/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  SriBalajiSaiRam_Chit
+//
+//  Created by kireeti on 09/02/19.
+//  Copyright © 2019 KireetiSoftSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
+
